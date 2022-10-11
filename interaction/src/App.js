e2684001c46ebb4226ced1830ff8572f74b5e0b4
ted-thuraa <mutwirited@gmail.com>
2022-10-11 16:03:49 +0300

    header

---
+++ interaction/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/header';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
